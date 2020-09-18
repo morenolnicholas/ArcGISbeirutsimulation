c24d94cf2f4c8721a35a508d67b8e8a7fdfb2b22
Nicholas Moreno <nicholaslonggeno.moreno@calbaptist.edu>
2020-09-18 16:45:11 -0700

    added the polyfill IE11 support

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import App from './App';
 import { setDefaultOptions } from 'esri-loader';
 import registerServiceWorker from './registerServiceWorker';
 import CalciteThemeProvider from 'calcite-react/CalciteThemeProvider';
+//Polyfill for IE
+import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
 setDefaultOptions({ css: true });
 
